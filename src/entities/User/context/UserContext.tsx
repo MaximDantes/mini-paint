f02bc0b03251ef9cc0da0f4354ec0ac3f2896bf2
MaximDantes <mfetisov2002@gmail.com>
2024-04-05 14:43:37 +0300

    feat: add auth redirect

--- src/entities/User/context/UserContext.tsx
+++ src/entities/User/context/UserContext.tsx
@@ -4,24 +4,31 @@ import { onAuthStateChanged, User } from 'firebase/auth'
 import { createContext, Dispatch, FC, ReactNode, SetStateAction, useContext, useEffect, useState } from 'react'
 import { firebaseAuth } from '@/shared/api/firebase'
 
-const Context = createContext<{ user: User | null; setUser: Dispatch<SetStateAction<User | null>> }>({
+const Context = createContext<{
+    user: User | null
+    setUser: Dispatch<SetStateAction<User | null>>
+    initialized: boolean
+}>({
     user: null,
     setUser: () => {},
+    initialized: false,
 })
 
 export const UserContext: FC<{ children: ReactNode }> = ({ children }) => {
     const [user, setUser] = useState<User | null>(null)
+    const [initialized, setInitialized] = useState(false)
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(firebaseAuth, (authUser) => {
             setUser(authUser)
+            setInitialized(true)
         })
         return () => {
             unsubscribe()
         }
     }, [setUser])
 
-    return <Context.Provider value={{ user, setUser }}>{children}</Context.Provider>
+    return <Context.Provider value={{ user, setUser, initialized }}>{children}</Context.Provider>
 }
 
 export const useUserContext = () => useContext(Context)
--- src/entities/User/utils/use-auth-redirect.ts
+++ src/entities/User/utils/use-auth-redirect.ts
@@ -5,12 +5,12 @@ import { useEffect } from 'react'
 import { useUserContext } from '@/entities/User'
 
 export const useAuthRedirect = () => {
-    const { user } = useUserContext()
+    const { user, initialized } = useUserContext()
     const router = useRouter()
 
     useEffect(() => {
-        if (!user) {
+        if (!user && initialized) {
             router.push('/sign-in')
         }
-    }, [user, router])
+    }, [user, initialized, router])
 }
